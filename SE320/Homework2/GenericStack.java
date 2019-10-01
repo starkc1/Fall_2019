21013a30573ac96d7dd5f69dca0dff0076cafdd9
Cameron Stark <starkc920@outlook.com>
2019-10-01 12:45:23 -0400

    added new functions

--- SE320/Homework2/GenericStack.java
+++ SE320/Homework2/GenericStack.java
@@ -9,7 +9,7 @@ public class GenericStack<E> {
         list = new Object[10];
     }
 
-    //Constructor 
+    //Constructor of specificed size
     public GenericStack(int size) {
         list = new Object[size];
     }
@@ -19,28 +19,110 @@ public class GenericStack<E> {
     }
 
     public E peek() {
-        if (list.length <= 0) {
+        if (getSize() <= 0) {
             return null;
         } else {
-            return list[0];
+            for (int i = 0; i < getSize(); i++) {
+                if (list[i] == null) {
+                    return (E)list[i-1];
+                } 
+            }
+        }
+        return null;
+    }
+
+    public void push(E item) {
+        Object[] newList;
+        if (list[getSize() - 1] != null) {
+            newList = new Object[getSize() * 2];
+
+            for (int i = 0; i < getSize(); i++) {
+                newList[i] = list[i];
+            }
+
+            newList[getSize()] = item;
+            list = newList;
+        } else {
+            for (int i = 0; i < getSize(); i++) {
+                if (list[i] == null) {
+                    list[i] = item;
+                    break;
+                } 
+            }
         }
     }
 
-    // public E push() {
-        
-    // }
+    public E pop() {
+        E poppedItem = null;
 
-    // public E pop() {
+        if (getSize() <= 0) {
+            return null;
+        } else {
+            for (int i = 0; i < getSize(); i++) {
+                if (list[i] == null) {
+                    poppedItem = (E)list[i - 1];
+                    list[i - 1] = null;
+                    break;   
+                }
+            }
+        }
 
-    // }
+        return poppedItem;
+    }
 
-    // public boolean isEmpty() {
+    public boolean isEmpty() {
+        boolean empty = true;
 
-    // }
+        for (int i = 0; i < getSize(); i++) {
+            if (list[i] != null) {
+                empty = false;
+            }
+        }
+
+        return empty;
+    }
+
+    @Override
+    public String toString() {
+        String arrayString = "";
+        for(int i = 0; i < list.length; i++) {
+            arrayString = arrayString + list[i] + " ";
+        }
+        return arrayString;
+    }
 
     public static void main(String[] args) {
         GenericStack<String> stack = new GenericStack();
 
-        System.out.println(stack);
+        System.out.println("Empty Default Stack: \n" + stack.toString()); 
+
+        stack.push("First");
+        stack.push("Second");
+        stack.push("Third");
+        stack.push("Fourth");
+        stack.push("Fifth");
+        stack.push("Sixtth");
+        stack.push("Seventh");
+        stack.push("Eighth");
+        stack.push("Ninth");
+        stack.push("Tenth");
+
+        System.out.println("\nFull Stack: \n" + stack.toString());
+
+        stack.push("Eleventh");
+
+        System.out.println("\nSize Expanded Stack: \n" + stack.toString());
+
+        String peekedItem = stack.peek();
+        System.out.println("\nPeeked Item Of Stack: \n" + peekedItem);
+
+        String poppedItem = stack.pop();
+        System.out.println("\nPopped Item Of Stack: \n" + poppedItem);
+        System.out.println("\nStack After Pop: \n" + stack.toString());
+
+
+        boolean isEmpty = stack.isEmpty();
+        System.out.println("\nIs Stack Empty: \n" + isEmpty);
+   
     }
 }
--- SE320/Homework2/Problem1.java
+++
@@ -1,9 +0,0 @@
-public class Problem1<E> {
-
-    private Array<E> list;
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
