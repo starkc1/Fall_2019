67b0a1424012a689c4bef084f6cd29f168c96be9
Cameron Stark <stark@Camerons-MacBook-Air.local>
2019-11-20 14:36:47 -0500

    updated code

--- CEC470/Project/main.c
+++ CEC470/Project/main.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+void convertHexToBin(unsigned char hex1);
+
+unsigned char memory[65536] = {0};
+unsigned char ACC = 0;
+unsigned char IR = 0;
+unsigned int MAR = 0;
+unsigned int PC = 0;
+
+unsigned char currentInstruction[24] = {0};
 int main() {
 
     //CREATE AND READ IN MEMORY FILE BEGIN
     FILE *file = NULL;
     int i = 0;
-    unsigned char memory[65536] = {0};
+    
 
     file = fopen("mem_in.txt", "r");
     
@@ -19,13 +28,26 @@ int main() {
     char c;
     while (!feof(file)) {
         c = getc(file);
-        memory[i] = c;
-        //printf("%c", memory[i]);
+
+        if (c == ' ') {
+
+        } else {
+            memory[i] = c;
+            //printf("%c", memory[i]);
+        }
+
         i++;
     }
     fclose(file);
     //CREATE AND READ IN MEMORY FILE END
 
+    //PROGRAM START
 
+    convertHexToBin(memory[0]);
+    //PROGRAM END
+}
 
+void convertHexToBin(unsigned char hex1) {
+    int value;
+    printf("%c", hex1);
 }
