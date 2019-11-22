5fed6aab7ff3e362853627fcdbffbb5ca03a4d1f
Cameron Stark <starkc920@outlook.com>
2019-11-22 09:46:17 -0500

    added stuff

--- CEC470/Project/main.c
+++ CEC470/Project/main.c
@@ -5,8 +5,8 @@
 #define HALT_OPCODE_a 0x1
 #define HALT_OPCODE_b 0x9
 
-void getBytes(unsigned char hex1, int byteNum);
-char convertHexToBin(hex);
+void getBytes(unsigned char hex, int byteNum);
+int * convertHexToBin(char hex);
 
 unsigned char memory[65536] = {0};
 unsigned char ACC = 0;
@@ -47,152 +47,155 @@ int main() {
 
     //PROGRAM START
     int PCtracker = PC;
+    
     while (memory[PCtracker] != HALT_OPCODE_a && memory[PCtracker + 1] != HALT_OPCODE_b) {
         int PCindexer = PC;
-
-
+        getBytes(memory[PCindexer], 1);
+        PC++;
+        PCtracker = PC;
     }
-    // while (memory[PC] != HALT_OPCODE) {
-    //     printf("%c", memory[PC]);
-    //     // getOpcode(memory[PC]);
-    //     // get2ndByte(memory[PC + 1]);
-    //     // get3rdByte(memory[PC + 2]);
-    //     PC++;
-    // }
     //PROGRAM END
 }
 
 void getBytes(unsigned char hex, int byteNum) {
+    int *binary = convertHexToBin((char) '0');
+    
+
+    for (int i = 0; i < 4; i++) {
+        printf("test %d", binary[i]);
+    }
+
     switch (byteNum) {
         case 1:
+            
             break;
         case 2:
             break;
     }
 }
 
-char convertHexToBin(hex) {
-    char bin[4];
-
+int * convertHexToBin(char hex) {
+    int* bin;
+    
     switch (hex) {
         case '0':
-            bin[0] = '0';
-            bin[1] = '0';
-            bin[2] = '0';
-            bin[3] = '0';
+            bin[0] = 0;
+            bin[1] = 0;
+            bin[2] = 0;
+            bin[3] = 0;
             break;
         
-        case '1':
-            bin[0] = '0';
-            bin[1] = '0';
-            bin[2] = '0';
-            bin[3] = '1';
-            break;
+        // case '1':
+        //     bin[0] = '0';
+        //     bin[1] = '0';
+        //     bin[2] = '0';
+        //     bin[3] = '1';
+        //     break;
         
-        case '2':
-            bin[0] = '0';
-            bin[1] = '0';
-            bin[2] = '1';
-            bin[3] = '0';
-            break;
+        // case '2':
+        //     bin[0] = '0';
+        //     bin[1] = '0';
+        //     bin[2] = '1';
+        //     bin[3] = '0';
+        //     break;
 
-        case '3':
-            bin[0] = '0';
-            bin[1] = '0';
-            bin[2] = '1';
-            bin[3] = '1';
-            break;
+        // case '3':
+        //     bin[0] = '0';
+        //     bin[1] = '0';
+        //     bin[2] = '1';
+        //     bin[3] = '1';
+        //     break;
         
-        case '4':
-            bin[0] = '0';
-            bin[1] = '0';
-            bin[2] = '1';
-            bin[3] = '0';
-            break;
+        // case '4':
+        //     bin[0] = '0';
+        //     bin[1] = '0';
+        //     bin[2] = '1';
+        //     bin[3] = '0';
+        //     break;
         
-        case '5':
-            bin[0] = '0';
-            bin[1] = '1';
-            bin[2] = '0';
-            bin[3] = '1';
-            break;
+        // case '5':
+        //     bin[0] = '0';
+        //     bin[1] = '1';
+        //     bin[2] = '0';
+        //     bin[3] = '1';
+        //     break;
 
-        case '6':
-            bin[0] = '0';
-            bin[1] = '1';
-            bin[2] = '1';
-            bin[3] = '0';
-            break;    
+        // case '6':
+        //     bin[0] = '0';
+        //     bin[1] = '1';
+        //     bin[2] = '1';
+        //     bin[3] = '0';
+        //     break;    
 
-        case '7':
-            bin[0] = '0';
-            bin[1] = '1';
-            bin[2] = '1';
-            bin[3] = '1';
-            break;
+        // case '7':
+        //     bin[0] = '0';
+        //     bin[1] = '1';
+        //     bin[2] = '1';
+        //     bin[3] = '1';
+        //     break;
 
-        case '8':
-            bin[0] = '1';
-            bin[1] = '0';
-            bin[2] = '0';
-            bin[3] = '0';
-            break;
+        // case '8':
+        //     bin[0] = '1';
+        //     bin[1] = '0';
+        //     bin[2] = '0';
+        //     bin[3] = '0';
+        //     break;
         
-        case '9':
-            bin[0] = '1';
-            bin[1] = '0';
-            bin[2] = '0';
-            bin[3] = '1';
-            break;
+        // case '9':
+        //     bin[0] = '1';
+        //     bin[1] = '0';
+        //     bin[2] = '0';
+        //     bin[3] = '1';
+        //     break;
         
-        case 'A':
-        case 'a':
-            bin[0] = '1';
-            bin[1] = '0';
-            bin[2] = '1';
-            bin[3] = '0';
-            break;
+        // case 'A':
+        // case 'a':
+        //     bin[0] = '1';
+        //     bin[1] = '0';
+        //     bin[2] = '1';
+        //     bin[3] = '0';
+        //     break;
         
-        case 'B':
-        case 'b':
-            bin[0] = '1';
-            bin[1] = '0';
-            bin[2] = '1';
-            bin[3] = '1';
-            break;
+        // case 'B':
+        // case 'b':
+        //     bin[0] = '1';
+        //     bin[1] = '0';
+        //     bin[2] = '1';
+        //     bin[3] = '1';
+        //     break;
 
-        case 'C':
-        case 'c':
-            bin[0] = '1';
-            bin[1] = '1';
-            bin[2] = '0';
-            bin[3] = '0';
-            break;
+        // case 'C':
+        // case 'c':
+        //     bin[0] = '1';
+        //     bin[1] = '1';
+        //     bin[2] = '0';
+        //     bin[3] = '0';
+        //     break;
 
-        case 'D':
-        case 'd':
-            bin[0] = '1';
-            bin[1] = '1';
-            bin[2] = '0';
-            bin[3] = '1';
-            break;
+        // case 'D':
+        // case 'd':
+        //     bin[0] = '1';
+        //     bin[1] = '1';
+        //     bin[2] = '0';
+        //     bin[3] = '1';
+        //     break;
 
-        case 'E':
-        case 'e':
-            bin[0] = '1';
-            bin[1] = '1';
-            bin[2] = '1';
-            bin[3] = '0';
-            break;
+        // case 'E':
+        // case 'e':
+        //     bin[0] = '1';
+        //     bin[1] = '1';
+        //     bin[2] = '1';
+        //     bin[3] = '0';
+        //     break;
 
-        case 'F':
-        case 'f':
-            bin[0] = '1';
-            bin[1] = '1';
-            bin[2] = '1';
-            bin[3] = '1';
-            break;
+        // case 'F':
+        // case 'f':
+        //     bin[0] = '1';
+        //     bin[1] = '1';
+        //     bin[2] = '1';
+        //     bin[3] = '1';
+        //     break;
     }
 
-    return bin;
+    return (bin);
 }
